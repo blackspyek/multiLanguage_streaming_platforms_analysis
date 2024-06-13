85415042115faee84034d7819e4acc6caed907da
zo <zosiaborys.zb@gmail.com>
2024-06-13 11:21:54 +0200

    Added colors to the map

--- map-app/src/map.js
+++ map-app/src/map.js
@@ -15,7 +15,8 @@ const COUNTRIES_AVG_URL = "/map/all";
 const MapChart = () => {
   const [hoveredMarker, setHoveredMarker] = useState(null);
 
-  const [countryRatings, setCountryRatings] = useState({});
+  const [countryRatings, setCountryRatings] = useState([]);
+  // const [countryRatings, setCountryRatings] = useState({});
 
   const [minRating, setMinRating] = useState(0);
   const [maxRating, setMaxRating] = useState(0);
@@ -51,6 +52,37 @@ const MapChart = () => {
       controller.abort();
     };
   }, [axiosPrivate]);
+
+  const getColorForRating = (rating) => {
+    const green = [0, 255, 0];
+    const yellow = [255, 255, 0];
+    const orange = [255, 165, 0];
+    const red = [255, 0, 0];
+
+    if (rating === undefined) return "rgb(255, 255, 255)";
+
+    const scale = (rating - minRating) / (maxRating - minRating);
+    if (scale <= 0.25) {
+      // Interpolate between red and orange
+      return interpolateColor(red, orange, scale / 0.25);
+    } else if (scale <= 0.5) {
+      // Interpolate between orange and yellow
+      return interpolateColor(orange, yellow, (scale - 0.25) / 0.25);
+    } else if (scale <= 0.75) {
+      // Interpolate between yellow and green
+      return interpolateColor(yellow, green, (scale - 0.5) / 0.25);
+    } else {
+      // Use green
+      return `rgb(${green[0]},${green[1]},${green[2]})`;
+    }
+  };
+
+  const interpolateColor = (color1, color2, factor) => {
+    const result = color1.map((c1, i) => c1 + factor * (color2[i] - c1));
+    return `rgb(${result[0]},${result[1]},${result[2]})`;
+  };
+
+
   return (
     <ComposableMap
       projectionConfig={{
@@ -73,47 +105,43 @@ const MapChart = () => {
           ))
         }
       </Geographies>
-      {countryRatings.length > 0 && // Conditional rendering check
+      {countryRatings.length > 0 &&
         markers
-          .filter(
-            (
-              { country } // Filter markers with ratings
-            ) =>
-              countryRatings.some(
-                (item) => item.country.toLowerCase() === country.toLowerCase()
-              )
+          .filter(({ country }) =>
+            countryRatings.some(
+              (item) => item.country.toLowerCase() === country.toLowerCase()
+            )
           )
-          .map(({ country, latitude, longitude }, index) => (
-            <Marker
-              key={index}
-              coordinates={[longitude, latitude]}
-              onMouseEnter={() => setHoveredMarker(index)}
-              onMouseLeave={() => setHoveredMarker(null)}
-            >
-              <circle r={2} fill={"red"} />
+          .map(({ country, latitude, longitude }, index) => {
+            const rating = countryRatings.find(
+              (item) => item.country.toLowerCase() === country.toLowerCase()
+            )?.rating;
+            return (
+              <Marker
+                key={index}
+                coordinates={[longitude, latitude]}
+                onMouseEnter={() => setHoveredMarker(index)}
+                onMouseLeave={() => setHoveredMarker(null)}
+              >
+                <circle r={2} fill={getColorForRating(rating)} />
 
-              {hoveredMarker === index && (
-                <text
-                  textAnchor="middle"
-                  y={10}
-                  style={{
-                    fontFamily: "system-ui",
-                    fill: "#FF0000",
-                    fontSize: 12,
-                    fontWeight: "bold",
-                  }}
-                >
-                  {country}:{" "}
-                  {countryRatings
-                    .find(
-                      (item) =>
-                        item.country.toLowerCase() === country.toLowerCase()
-                    )
-                    ?.rating.toFixed(2)}
-                </text>
-              )}
-            </Marker>
-          ))}
+                {hoveredMarker === index && (
+                  <text
+                    textAnchor="middle"
+                    y={10}
+                    style={{
+                      fontFamily: "system-ui",
+                      fill: "#FF0000",
+                      fontSize: 12,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {country}: {rating?.toFixed(2)}
+                  </text>
+                )}
+              </Marker>
+            );
+          })}
     </ComposableMap>
   );
 };
